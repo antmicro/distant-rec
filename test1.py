bad28ff78cf5deeee48fb02b8ef72783c93cf6aa
Adam Olech <aolech@internships.antmicro.com>
2019-11-22 13:06:27 +0000

    Flush

--- test1.py
+++ test1.py
@@ -7,3 +7,5 @@ if __name__ == '__main__':
     channel = grpc.insecure_channel('localhost:50051')
     upme = Uploader(channel)
     print(upme.upload_directory(sys.argv[1]))
+    upme.flush()
+    upme.close()
