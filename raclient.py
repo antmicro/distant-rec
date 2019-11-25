b938e3a83981545ff0c9e01a667686280ee810a4
Adam Olech <aolech@internships.antmicro.com>
2019-11-25 12:30:09 +0000

    Cleaned up the code somewhat

---
+++ raclient.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import grpc, sys
+from buildgrid.client.cas import Uploader, Downloader
+from buildgrid._protos.build.bazel.remote.execution.v2 import remote_execution_pb2, remote_execution_pb2_grpc
+
+class RAC:
+    def __init__(self, uri):
+        self.channel = grpc.insecure_channel(uri)
+        self.uploader = Uploader(self.channel)
+        self.downloader = Downloader(self.channel)
+
+    def upload_action(self, commands, input_root, output_file, cache=True):
+        command_handler = remote_execution_pb2.Command()
+
+        for arg in commands:
+            command_handler.arguments.extend([arg])
+
+        for ofile in output_file:
+            print([ofile])
+            command_handler.output_files.extend([ofile])
+
+        command_digest = self.uploader.put_message(command_handler, queue=True)
+
+        input_root_digest = self.uploader.upload_directory(input_root)
+
+        action = remote_execution_pb2.Action(command_digest=command_digest,
+                input_root_digest = input_root_digest,
+                do_not_cache=not cache)
+
+        action_digest = self.uploader.put_message(action, queue=True)
+
+        return action_digest
+
+    def run_command(self, action_digest, cache=True):
+        stub = remote_execution_pb2_grpc.ExecutionStub(self.channel)
+
+        request = remote_execution_pb2.ExecuteRequest(instance_name="",
+                action_digest=action_digest,
+                skip_cache_lookup=not cache)
+
+        response = stub.Execute(request)
+
+        stream = None
+
+        for stream in response:
+            print(stream)
+
+        execute_response = remote_execution_pb2.ExecuteResponse()
+        stream.response.Unpack(execute_response)
+
+        return execute_response.result.output_files
+
+if __name__ == '__main__':
+    test = RAC('localhost:50051')
+    ad = test.upload_action(['./test.sh'], sys.argv[1], ('hello',))
+    test.uploader.flush()
+    ofiles = test.run_command(ad)
+    print(ofiles)
+
+    for blob in ofiles:
+        test.downloader.download_file(blob.digest, blob.path, is_executable=blob.is_executable)
+
+    test.uploader.close()
+    test.downloader.close()
