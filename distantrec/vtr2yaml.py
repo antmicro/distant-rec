43da4494159f5cd6902ae85c87b45dfba50da26b
Adam Olech <aolech@internships.antmicro.com>
2019-12-03 15:28:40 +0100

    Added method for temporary dir fix

--- distantrec/vtr2yaml.py
+++ distantrec/vtr2yaml.py
@@ -2,6 +2,7 @@
 
 class ScriptMangler:
     def __init__(self, path):
+        self.path = path
         self.file_handle = open(path, "r+")
         self.file_string = self.file_handle.read()
         self.file_list = self.file_string.split("\n")
@@ -12,6 +13,17 @@ class ScriptMangler:
 
         for i in range(4,7):
             arguments[i] = self.__atr_text(arguments[i])
+
+        arguments_join = " ".join(arguments)
+
+        self.file_list[13] = arguments_join
+
+    def temporary_folder_fix(self):
+        temp_relative_path = __atr_text(self.path)
+        
+        arguments = self.file_list[13].split(" ")
+
+        arguments[14] = temp_relative_path
 
         arguments_join = " ".join(arguments)
 
