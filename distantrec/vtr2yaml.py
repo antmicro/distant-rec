61e5dc86ed6a6cd5609a2f3c02633d497d1c20cc
Adam Olech <aolech@internships.antmicro.com>
2019-12-04 13:00:14 +0100

    Don't add make

--- distantrec/vtr2yaml.py
+++ distantrec/vtr2yaml.py
@@ -26,9 +26,9 @@ def buildyaml(scripts, cores=1):
         rel = atr_text(script)
         yaml[rel] = yaml_section(rel)
 
-    yaml["all"] = yaml_section("phony", ["make"]+list(yaml.keys()))
+    yaml["all"] = yaml_section("phony", list(yaml.keys()))
 
-    yaml["make"] = yaml_section("make -j "+cores)
+#    yaml["make"] = yaml_section("make -j "+cores)
 
     return yaml
 
