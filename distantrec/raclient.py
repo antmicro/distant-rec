aa9f479ea357197c69c73c6c4640926a67a8e3ed
Tomasz Gielda <tgielda@antmicro.com>
2019-12-10 13:30:29 +0100

    No of threads in config

--- distantrec/raclient.py
+++ distantrec/raclient.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+from distantrec.helpers import get_option
 
 NO_SERVER = 0
 if len(sys.argv) > 3:
@@ -14,6 +15,6 @@ from distantrec.helpers import get_option
 
 
 def main():
-    threads_amount = 10
+    threads_amount = int(get_option('SETUP','THREADS'))
     b = BuildRunner(sys.argv[1])
     b.run(sys.argv[2], threads_amount)
