5b2ec8985b2b5d4b591b23c42f1c0db5149824b5
Robert Winkler <rwinkler@antmicro.com>
2019-12-13 20:37:43 +0100

    Add dumping dependency tree to cache

--- distantrec/DepTree.py
+++ distantrec/DepTree.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import yaml, anytree
+import yaml, anytree, pickle, hashlib, os
 from threading import Lock, Condition
 from pprint import pprint
 
@@ -11,7 +11,7 @@ def measure_time(fun, *args):
     ret = fun(*args)
     end = time()
 
-    print("    Elapsed time: " + str(round(end - start, 2)))
+    print("  Elapsed time: " + str(round(end - start, 2)))
     return ret
 
 class DepNode(anytree.NodeMixin):
@@ -50,20 +50,32 @@ class DepNode(anytree.NodeMixin):
             return super().__eq__(other)
 
 class DepTree:
-    def __init__(self, yaml_path, target):
-
+    def __init__(self, yaml_path, target, init=True):
         self._depyaml = None    # YAML file dictionary
         self._deproot = None    # Dependency tree
-
-        self._tree_lock = Lock()
-        self._build_list_lock = Lock()
-        self._ready_lock = Lock()
-        self._leaves_ready = Condition()
         self._nodes_dict = {}   # {target: [all_related_nodes]}
         self._ready_list = []
         self._leaves_dict = {}  # {node: 'node_target'}
         self._build_list = []   # Nodes that are in building
 
+        if init == True:
+            self._tree_lock = Lock()
+            self._build_list_lock = Lock()
+            self._ready_lock = Lock()
+            self._leaves_ready = Condition()
+
+            print("Checking cache...")
+            is_cache_up_to_date = measure_time(self._cache_is_up_to_date, yaml_path, target)
+            if (is_cache_up_to_date == True):
+                print("Loading dependency tree from cache...")
+                measure_time(self._load_tree_from_cache, yaml_path, target)
+            else:
+                print("Standard initialization...")
+                self._standard_init(yaml_path, target)
+
+    ### HELPER METHODS ###
+
+    def _standard_init(self, yaml_path, target):
         print("Parsing YAML input file...")
         with open(yaml_path) as fd:
             self._depyaml = measure_time(yaml.safe_load, fd)
@@ -76,7 +88,10 @@ class DepTree:
         print("Prepare leaves...")
         measure_time(self._prepare_leaves)
 
-    ### HELPER METHODS ###
+        cache_name = self._get_cache_name(yaml_path, target)
+        print("Saving dependency tree to %s ..." % cache_name)
+        if not os.path.exists(cache_name):
+            measure_time(self._save_tree_to_cache, yaml_path, target)
 
     def _prepare_leaves(self):
         for leaf in self._deproot.leaves:
@@ -147,6 +162,72 @@ class DepTree:
 
                 parent.children = parent_children_list
 
+    ### CACHE ###
+
+    def _save_obj(self, obj, path):
+        with open(path, 'wb') as fh:
+            pickle.dump(obj, fh)
+
+    def _read_obj(self, path):
+        with open(path, 'rb') as fh:
+            ret = pickle.load(fh)
+        return ret
+
+    def _load_tree_from_cache(self, yaml_path, target):
+        cache_file_path = self._get_cache_name(yaml_path, target)
+        dep = self._read_obj(cache_file_path)
+        self._depyaml      = dep._depyaml
+        self._deproot      = dep._deproot
+        self._nodes_dict   = dep._nodes_dict
+        self._ready_list   = dep._ready_list
+        self._leaves_dict  = dep._leaves_dict
+        self._build_list   = dep._build_list
+        self._cache_hash   = dep._cache_hash
+
+    def _save_tree_to_cache(self, yaml_path, target):
+        dep = DepTree("", "", init=False)
+
+        dep._depyaml      = self._depyaml
+        dep._deproot      = self._deproot
+        dep._nodes_dict   = self._nodes_dict
+        dep._ready_list   = self._ready_list
+        dep._leaves_dict  = self._leaves_dict
+        dep._build_list   = self._build_list
+        dep._cache_hash   = self._get_dep_hash(yaml_path, target)
+
+        cache_name = self._get_cache_name(yaml_path, target)
+        self._save_obj(dep, cache_name)
+
+    def _hash_file(self, path, block_size=65536):
+        hasher = hashlib.sha1()
+        with open(path, 'rb') as afile:
+            buf = afile.read(block_size)
+            while len(buf) > 0:
+                hasher.update(buf)
+                buf = afile.read(block_size)
+        return hasher.hexdigest()
+
+    def _get_dep_hash(self, yaml_file, target):
+        yaml_hash = self._hash_file(yaml_file)
+        to_hash = (str(yaml_hash) + target).encode('utf-8')
+        final_hash = hashlib.sha1(to_hash).hexdigest()
+        return final_hash
+
+    def _get_cache_name(self, yaml_file, target):
+        final_hash = self._get_dep_hash(yaml_file, target)
+        return ".%s.cache" % str(final_hash)
+
+    def _cache_is_up_to_date(self, yaml_file, target):
+        cache_file_name = self._get_cache_name(yaml_file, target)
+
+        if os.path.exists(cache_file_name):
+            dep = self._read_obj(cache_file_name)
+            file_hash = self._get_dep_hash(yaml_file, target)
+            if (dep._cache_hash == file_hash):
+                return True
+        else:
+            return False
+
     ### PUBLIC API ###
 
     def print_tree(self, start_node = None):
@@ -196,3 +277,10 @@ class DepTree:
                 self._build_list.append(result)
 
             return result
+
+def main():
+    dep = DepTree("../out.yml", "all")
+    dep.print_tree()
+
+if __name__ == "__main__":
+    main()
