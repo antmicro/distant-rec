8f1db903212313b344803c105fd6ba5b0dfe7fdd
Robert Winkler <rwinkler@antmicro.com>
2019-12-13 16:59:36 +0100

    Delete absolute part of path, fix commands

--- distantrec/dep2yaml.py
+++ distantrec/dep2yaml.py
@@ -134,7 +134,6 @@ class Dep2YAML:
             for part in rule_parts:
                 if "$" in part:
                     var = part.replace("$", '')
-                    var = var.replace("'", "")
                     if var in definitions:
                         rule = rule.replace(part, definitions[var])
         elif rule in definitions:
@@ -152,6 +151,9 @@ class Dep2YAML:
         rule = self._format_rule(rule)
         rule = self._remove_wrong_cmake_calls(rule)
         rule = self._format_rule(rule)
+
+        rule = rule.replace(self.root_dir + "/", '')
+        rule = rule.replace(self.root_dir, "./")
 
         return rule
 
@@ -240,23 +242,6 @@ class Dep2YAML:
                                         wb_definitions,
                                         dependencies)
 
-        # Convert paths in resolved rule to relative
-        rule_parts = wb_rule.split(' ')
-        for part in rule_parts:
-            if "=" in part:
-                split_list = part.split("=")
-                if len(split_list) != 2:
-                    continue
-                else:
-                    [variable, tmp_path] = split_list
-                    if os.path.exists(tmp_path) and os.path.isabs(tmp_path):
-                        new_path = self._convert_to_relative_path(tmp_path)
-                        wb_rule = wb_rule.replace(tmp_path, new_path)
-            elif os.path.isabs(part):
-                if self._bellongs_to_project(part):
-                    new_path = self._convert_to_relative_path(part)
-                    wb_rule = wb_rule.replace(part, new_path)
-
         if len(wb_output_list) == 1:
             self._write_to_yaml(wb_explicit_deps_list, wb_output_list, wb_rule, wb_implicit_deps_list)
 
@@ -273,11 +258,15 @@ class Dep2YAML:
         return result.named["rule"]
 
     def _get_variable_definition(self, line):
+
         result = parse("{option} '{variable}' = {definition}", line)
         assert result != None
 
         variable = result.named["variable"]
-        definition = result.named["definition"].strip("'")
+        if result.named["definition"] == "''":
+            definition = ''
+        else:
+            definition = result.named["definition"][1:-1]
 
         definition_parts = definition.split(' ')
         for part in definition_parts:
