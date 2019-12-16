0786b1b954785de89206270d107a9c3ce68b96b3
Adam Olech <aolech@internships.antmicro.com>
2019-12-16 14:09:48 +0100

    Changed helper location

--- tools/vtr-helper.sh
+++ tools/vtr-helper.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+cd vtr-verilog-to-routing
+make
+./vtr_flow/scripts/run_vtr_task.pl -d -l vtr_flow/tasks/regression_tests/$1/task_list.txt
+
+if [ $1 = "vtr_reg_nightly" ] || [ $1 = "vtr_reg_nightly" ]; then
+	cd build
+	make
+	cd ..
+	./dev/upgrade_vtr_archs.sh
+fi
+
 find . -name "*.o" -type f -delete
 find . -name "*.h" -type f -delete
 find . -name "*.cpp" -type f -delete
@@ -14,13 +25,10 @@ find . -name "*.make" -type f -delete
 find . -name "*.rst" -type f -delete
 find . -name "*.html" -type f -delete
 rm -rf .git
-make
-./vtr_flow/scripts/run_vtr_task.pl -d -l vtr_flow/tasks/regression_tests/$1/task_list.txt
-
-if [ $1 = "vtr_reg_nightly" ] || [ $1 = "vtr_reg_nightly" ]; then
-	cd build
-	make
-	cd ..
-fi
 
 find . -type l -exec rm -f {} \;
+
+mv build/abc/abc abc/abc && \
+mv build/vpr/vpr vpr/vpr && \
+mv build/ace2/ace ace2/ace && \
+mv  build/ODIN_II/odin_II ODIN_II/odin_II
