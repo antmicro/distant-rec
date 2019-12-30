1c2ec536f5f4ed8da7141db0b78dda84676585bc
Adam Olech <aolech@internships.antmicro.com>
2019-12-30 14:10:44 +0000

    Added symbi abs2rbe

---
+++ tools/symbi-abs2rbe.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+RBE="/b/w/f"
+P1="${PWD//\//\\/}"
+P2="${RBE//\//\\/}"
+
+echo "Your current directory is ${PWD}. We'll change all occurences of it to ${RBE}."
+
+read -p "Are you sure? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	echo "Starting substitution..."
+	grep -rl "$PWD" build/env/conda --binary-file=without-match | xargs sed -i "s/$P1/$P2/g"
+fi
